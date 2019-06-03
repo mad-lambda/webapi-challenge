2cddddbb0a77e84008520ccd6f71d7f7c1a303ad
Marc de la Torre <mdelatorre1314@gmail.com>
2019-06-03 13:14:21 -0700

    routers and index.js set up for CRUD operations

---
+++ data/routers/actions.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const aData = require('../helpers/actionModel.js');
+
+const router = express.Router();
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const port = 1314;
 
 const server = express();
+const ProjectRoutes = require('./data/routers/projects.js')
+const ActionRoutes = require('./data/routers/actions.js')
 
 server.use(express.json());
 
+server.use('/api/projects', ProjectRoutes);
+server.use('/api/actions', ActionRoutes);
 
 server.listen(port, () => console.log('\n** Server Running on http://localhost:1314 **\n'))
