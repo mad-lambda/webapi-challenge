43b17e23f6ec5db3416c4f4f6e938a8a0425a3f2
Marc de la Torre <mdelatorre1314@gmail.com>
2019-06-03 13:38:10 -0700

    get requests for projects built and tested

--- data/routers/projects.js
+++ data/routers/projects.js
@@ -4,4 +4,30 @@ const pData = require('../helpers/projectModel.js');
 
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+    pData.get()
+         .then(projects => {
+             res.status(200).json(projects);
+         })
+         .catch(error => {
+             res.status(500).json({error: "Error retrieving project"})
+         })
+});
+
+router.get('/:id', (req, res) => {
+    const id =req.params.id;
+    pData.get(id)
+         .then(project => {
+             if(project){
+                 res.status(200).json(project);
+             } else {
+                 res.status(404).json({error: "The specified ID does not exist"})
+             }
+         })
+         .catch(error => {
+             res.status(500).json({error: "Error retrieving project"})
+         })
+});
+
 module.exports = router;
